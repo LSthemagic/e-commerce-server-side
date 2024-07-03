03d895c6446a3b1cfa0e80c2d0d0cac268ea5eae
Railan Santana <lansilvah14fsa@gmail.com>
2024-07-03 11:13:49 -0300

    feat: Stock entity

--- src/main/java/com/railansantana/e_commerce/domain/Stock.java
+++ src/main/java/com/railansantana/e_commerce/domain/Stock.java
@@ -1,2 +1,41 @@
-package com.railansantana.e_commerce.domain;public class Stock {
+package com.railansantana.e_commerce.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Document
+public class Stock implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    @DBRef
+    private Product product;
+    private int quantity;
+
+    public void addStock(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        this.quantity -= quantity;
+        if (this.quantity < 0) {
+            this.quantity = 0;
+        }
+    }
+
+    public Boolean verifyStock(int quantity) {
+        return this.quantity >= quantity;
+    }
+
 }
