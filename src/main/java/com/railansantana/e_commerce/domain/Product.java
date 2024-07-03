27bd3cbe6313d0b77910301953062fd6840512b9
Railan Santana <lansilvah14fsa@gmail.com>
2024-07-03 11:13:03 -0300

    feat: product entity

--- src/main/java/com/railansantana/e_commerce/domain/Product.java
+++ src/main/java/com/railansantana/e_commerce/domain/Product.java
@@ -1,2 +1,50 @@
-package com.railansantana.e_commerce.domain;public class Product {
+package com.railansantana.e_commerce.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Document
+public class Product implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+    private String image;
+
+
+    public Double calculateDiscount(double percent) {
+        return price * (percent / 100);
+    }
+
+    public void insertDiscount(double percent) {
+        setPrice(calculateDiscount(percent));
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
